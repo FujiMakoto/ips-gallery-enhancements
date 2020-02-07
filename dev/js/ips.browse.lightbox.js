4fb27ac6db3bdc787ce3178bc85ddc1fbe90a856
Makoto <FujiMakoto@users.noreply.github.com>
2020-02-07 09:14:56 -0500

    Same as last commit

--- dev/js/ips.browse.lightbox.js
+++ dev/js/ips.browse.lightbox.js
@@ -1,32 +1,11 @@
-ips.controller.mixin('lightboxTrigger', 'gallery.front.browse.imageLightbox', true, function () {
-
-    /**
-     * Initialize controller
-     *
-     * @returns {void}
-     */
-    // this.initialize = function () {
-    //     this.on( 'click', '[data-imageLightbox]', this.launchLightbox );
-    //     this.on( document, 'keydown', this.keyDown );
-    //
-    //     // Primary event that watches for URL changes
-    //     History.Adapter.bind( window, 'statechange', _.bind( this.stateChange, this ) );
-    //
-    //     this.setup();
-    // };
-
-    /**
-     * Disable the lightbox on configured pages
-     *
-     * @param	e	Event
-     * @return void
-     */
-    this.around('launchLightbox', function (origFn, e) {
+ips.controller.mixin('lightboxTrigger', 'gallery.front.browse.imageLightbox', true, function() {
+    this.setup = function() {
+        Debug.info('Suppressing lightbox')
+    };
+    this.around('launchLightbox', function(origFn, e) {
         console.log('Launching lightbox');
-        if ( !ips.getSetting('gallerye_noLightbox') )
-        {
+        if (!ips.getSetting('genh_noLightbox')) {
             origFn(e)
         }
     });
-
-});
+});;
